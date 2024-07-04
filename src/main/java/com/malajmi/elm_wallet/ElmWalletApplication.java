826c2535224ec50486726cea36045a9544a0efa3
Mishal-Alajmi <mishalajmi.dev@gmail.com>
2024-07-04 23:04:26 +0300

    Initial Commit

---
+++ src/main/java/com/malajmi/elm_wallet/ElmWalletApplication.java
@@ -0,0 +1,13 @@
+package com.malajmi.elm_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malajmi/elm_wallet/ElmWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malajmi.elm_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
